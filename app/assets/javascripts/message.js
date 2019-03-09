670d7fc75043988cf58c423069cca31092934dc2
usualpn0221 <usualpn@gmail.com>
2019-03-09 16:03:52 +0900

    改行とインデント修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,26 +1,18 @@
 $(function(){
    function buildHTML(message){
+    var kari = `<div class="chat__user"><span style="font-size:16px; font-family:bold; color:#434A54";>
+                ${message.user_name}</span>
+                ${message.date}
+                </div>
+                <div class="chat__chat">
+               <p class="lower-message__content">
+                ${message.body}</p>`
 
+    if (message.image.url !== null){
+      var kari = `${kari}<img src="${message.image.url}" class="lower-message__image">`
+    }
 
-    var kari = `
-                    <span style="font-size:16px; font-family:bold; color:#434A54";>${message.user_name}</span>
-                    ${message.date}
-                    </div>
-
-                  <div class="chat__chat">
-
-                   <p class="lower-message__content">
-                  ${message.body}</p>
-
-                  `
-     if (message.image.url !== null){
-    var kari = ` ${kari}<img src="${message.image.url}" class="lower-message__image">`
-                  }
-
-    var html =`<div class="chat__user">${kari}</div>`
-
-
-
+    var html =`${kari}</div>`
 
     return html;
   }
@@ -37,21 +29,15 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
-
-      })
-
+    })
 
     .done(function(data){
-
       var html = buildHTML(data);
       $('.chat__wrapper').append(html)
-
       $('.new_message')[0].reset()
 
       var message = $('.chat__wrapper')[0].scrollHeight
-
-    $('.chat__wrapper').animate({scrollTop: message});
-
+      $('.chat__wrapper').animate({scrollTop: message});
     })
 
     .fail(function(){
